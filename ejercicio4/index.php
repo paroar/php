9c5bdcc70d69fea5373cd28989033cf74d94ae11
unknown <Usuario@AU8BISPC12.ieszayas.org>
2019-09-23 09:54:17 +0200

    Joined php files & completed form/css

--- ejercicio4/index.php
+++ ejercicio4/index.php
@@ -10,24 +10,31 @@ echo <<<EOD
 </head>
 
 <body>
+<div class="header">
+<div class="box-link-table">
+    <h1 class="heading-primary">
+        <span class="heading-primary-main">Tabla de multiplicar</span>
+    </h1>
+    <form action="/php/ejercicio4/index.php" method="POST" class="form">
+        <input type="text" name="x" class="input-number" value="{$_POST['x']}">
+    </form>
 EOD;
-
-if(!isset($_POST["x"])){
-    echo <<<EOD
-    <div class="header">
-        <div class="box-link-table">
-            <h1 class="heading-primary">
-                <span class="heading-primary-main">Tabla de multiplicar</span>
-            </h1>
-            <form action="php/tablaMultiplicar.php" method="POST" class="form">
-                <input type="text" name="x" class="input-number" value="0">
-            </form>
-        </div>
-    </div>
+if(isset($_POST["x"])){
+    $num = $_POST['x'];
+    echo "<table>";
+    for($i = 1; $i < 11 ;$i++){
+            $pro = $i*$num;
+            echo <<<EOD
+            <tr>
+                <td>$i x $num</td>
+                <td>{$pro}</td>
+            </tr>
 EOD;
+    }
+    echo "</table>";
 }
-exec ('php/tablaMultiplicar.php');
 echo <<<EOD
+</div>
 </body>
 </html>
 EOD;
