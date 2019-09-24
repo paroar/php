7d056df6b5e2373a07846b7f68e66fa065562d9a
paroar <prodriguezarmida@gmail.com>
2019-09-24 17:13:23 +0200

    Having problems initializing counter

--- ejercicio7/index.php
+++ ejercicio7/index.php
@@ -1,5 +1,3 @@
-<?php
-echo <<<EOD
 <!DOCTYPE html>
 <html>
 
@@ -15,8 +13,8 @@ echo <<<EOD
     <h1 class="heading-primary">
         <span>Irregular verbs</span>
     </h1>
-EOD;
 
+<?php
 $arr = array(
     array("perdonar","forgive","forgave","forgiven"),
     array("empezar","begin","began","begun"),
@@ -29,36 +27,48 @@ $arr = array(
     array("encontrar","find","found","found"),
     array("volar","fly","flew","flown")
 );
-
-$arrCompleted = array();
-
-if(count($arr) > 0){
+echo isset($_POST['contador']);
+if(isset($_POST['contador'])){
+    $num = $_POST['contador'];
     echo <<<EOD
     <div class="box-verb">
-        <span class="box-verb-primary">{$arr[0][0]}</span><br>
-        <form class="form" method="" action="">
-            <input type="text" class="input-box" name="infinitive" onfocus="this.value=''" value="{$arr[0][1]}" required></input>
-            <input type="text" class="input-box" name="simple" onfocus="this.value=''" value="{$arr[0][2]}" required></input>
-            <input type="text" class="input-box" name="participle" onfocus="this.value=''" value="{$arr[0][3]}" required></input>
+        <span class="box-verb-primary">{$arr[$num][0]}</span><br>
+        <form class="form" method="post" action="{$_SERVER['PHP_SELF']}">
+            <input type="text" class="input-box" name="infinitive" value="{$arr[$num][1]}" required></input>
+            <input type="text" class="input-box" name="simple" value="{$arr[$num][2]}" required></input>
+            <input type="text" class="input-box" name="participle" value="{$arr[$num][3]}" required></input>
             <input type="submit" class="submit" value="Send">
         </form>
     </div>
 EOD;
-
-if(
-    $_REQUEST['infinitive'] == $arr[0][1] &&
-    $_REQUEST['simple'] == $arr[0][2] &&
-    $_REQUEST['participle'] == $arr[0][3] 
-    ){
-    $aciertos += 1; 
+    $_POST['contador'] += 1;
+    echo $_POST['contador'];
 }else{
-    $errores += 1;
-}
-print_r($arrCompleted);
+    $_POST['contador'] = 0;
+    $_POST['aciertos'] = 0;
+    $_POST['errores'] = 0;
+    $_POST['infinitive'] = "";
+    $_POST['simple'] = "";
+    $_POST['participle'] = "";
+    $_POST['contador'] += 1;
+    echo $_POST['contador'];
+    echo <<<EOD
+    <form class="form" method="post" action="{$_SERVER['PHP_SELF']}">
+        <input type="submit" class="submit" value="Start">
+    </form>
+EOD;
 }
 
-echo <<<EOD
+/*if(
+    isset($_POST['contador']) &&
+    $_REQUEST['infinitive'] == $arr[$num[1]] &&
+    $_REQUEST['simple'] == $arr[$num[2]] &&
+    $_REQUEST['participle'] == $arr[$num[3]]
+){
+    $_REQUEST['aciertos'] += 1; 
+}else{
+    $_REQUEST['errores'] += 1;
+}*/
+?>
 </body>
 </html>
-EOD;
-?>
