a83547263ad5ab4a900210b21fcd58e2d76a56d6
paroar <prodriguezarmida@gmail.com>
2019-09-25 23:53:27 +0200

    Still with the same issues

--- ejercicio7/index.php
+++ ejercicio7/index.php
@@ -16,6 +16,7 @@
 
 <?php
 $arr = array(
+    array("","",""),
     array("perdonar","forgive","forgave","forgiven"),
     array("empezar","begin","began","begun"),
     array("doblar","bend","bent","bent"),
@@ -27,48 +28,59 @@ $arr = array(
     array("encontrar","find","found","found"),
     array("volar","fly","flew","flown")
 );
-echo isset($_POST['contador']);
-if(isset($_POST['contador'])){
-    $num = $_POST['contador'];
+
+if(isset($_REQUEST['contador']) && $_REQUEST['contador'] < count($arr) - 1){
+    $num = $_REQUEST['contador'] + 1;
+    print_r($_REQUEST);
+    echo "{$arr[$num-1][1]} . {$arr[$num][1]}";
+    if(
+        $_REQUEST['infinitive'] === $arr[$num-1][1] &&
+        $_REQUEST['simple'] === $arr[$num-1][2]
+    ){
+        $_REQUEST['aciertos'] += 1; 
+    }else{
+        $_REQUEST['errores'] += 1;
+    }
     echo <<<EOD
     <div class="box-verb">
         <span class="box-verb-primary">{$arr[$num][0]}</span><br>
-        <form class="form" method="post" action="{$_SERVER['PHP_SELF']}">
+        <form class="form" method="get" action="{$_SERVER['PHP_SELF']}">
             <input type="text" class="input-box" name="infinitive" value="{$arr[$num][1]}" required></input>
-            <input type="text" class="input-box" name="simple" value="{$arr[$num][2]}" required></input>
-            <input type="text" class="input-box" name="participle" value="{$arr[$num][3]}" required></input>
+            <input type="text" class="input-box" name="simple" value="{$arr[$num][1]}" required></input>
+            <input type="text" class="input-box" name="participle" value="{$arr[$num][1]}" required></input>
+            <input type="hidden" id="custId" name="contador" value="{$num}">
+            <input type="hidden" name="aciertos" value="{$_REQUEST['aciertos']}">
+            <input type="hidden" name="errores" value="{$_REQUEST['errores']}">
             <input type="submit" class="submit" value="Send">
         </form>
     </div>
 EOD;
-    $_POST['contador'] += 1;
-    echo $_POST['contador'];
-}else{
-    $_POST['contador'] = 0;
-    $_POST['aciertos'] = 0;
-    $_POST['errores'] = 0;
-    $_POST['infinitive'] = "";
-    $_POST['simple'] = "";
-    $_POST['participle'] = "";
-    $_POST['contador'] += 1;
-    echo $_POST['contador'];
+}elseif(!isset($_REQUEST['contador'])){
     echo <<<EOD
-    <form class="form" method="post" action="{$_SERVER['PHP_SELF']}">
+    <form class="form" method="get" action="{$_SERVER['PHP_SELF']}">
+        <input type="hidden" name="contador" value="0">
+        <input type="hidden" name="aciertos" value="0">
+        <input type="hidden" name="errores" value="0">
+        <input type="hidden" name="infinitive" value="">
+        <input type="hidden" name="simple" value="">
+        <input type="hidden" name="participle" value="">
         <input type="submit" class="submit" value="Start">
     </form>
 EOD;
-}
-
-/*if(
-    isset($_POST['contador']) &&
-    $_REQUEST['infinitive'] == $arr[$num[1]] &&
-    $_REQUEST['simple'] == $arr[$num[2]] &&
-    $_REQUEST['participle'] == $arr[$num[3]]
-){
-    $_REQUEST['aciertos'] += 1; 
 }else{
-    $_REQUEST['errores'] += 1;
-}*/
+    echo <<<EOD
+    <form class="form" method="get" action="{$_SERVER['PHP_SELF']}">
+        <input type="hidden" name="contador" value="0">
+        <input type="hidden" name="aciertos" value="0">
+        <input type="hidden" name="errores" value="0">
+        <input type="hidden" name="infinitive" value="">
+        <input type="hidden" name="simple" value="">
+        <input type="hidden" name="participle" value="">
+        <input type="submit" class="submit" value="Restart">
+    </form>
+EOD;
+}
 ?>
+</div>
 </body>
 </html>
