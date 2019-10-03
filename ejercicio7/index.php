788213cd824e031facc9b3af75afb376dfcb0415
paroar <prodriguezarmida@gmail.com>
2019-10-03 18:13:58 +0200

    fixed accumulator

--- ejercicio7/index.php
+++ ejercicio7/index.php
@@ -3,42 +3,68 @@
 
 <head>
     <link rel="stylesheet" href="css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 </head>
 
 <body>
-<div class="header">
-<img src="img/logo.jpg" alt="logo" heigth="100px" width="100px" class="logo">
-<div class="box-link-table">
-    <h1 class="heading-primary">
-        <span>Irregular verbs</span>
-    </h1>
+    <div class="header">
+        <img src="img/logo.jpg" alt="logo" heigth="100px" width="100px" class="logo">
+        <div class="box-link-table">
+            <h1 class="heading-primary">
+                <span>Irregular verbs</span>
+            </h1>
 
-<?php
-include("php/verbos.php");
-include("php/utility_functions.php");
-if(isset($_REQUEST['submit'])){
-    echo "AQUÍ VA EL CONTADOR -> " . $_REQUEST['contador'] . " <-";
-    $contador = $_REQUEST['contador'];
-    if(
-        ($_REQUEST['infinitive'] === $arr[$num][1]) and 
-        ($_REQUEST['simple'] === $arr[$num][2]) and 
-        ($_REQUEST['participle'] === $arr[$num][3])
-    ){
-        $_REQUEST['aciertos'] += 1;
-    }else{
-        $_REQUEST['errores'] += 1;
-    }
-    $num += 1;
-    pintarArgumento($arr, $contador, $aciertos);
-}elseif(!isset($_REQUEST['contador'])){
-    pintar();
-}else{
-    echo "Aciertos: {$_REQUEST['aciertos']} <br>";
-    echo "Errores: {$_REQUEST['errores']}";
-    pintar();
-}
-?>
-</div>
+            <?php
+            include("php/verbos.php");
+            include("php/utility_functions.php");
+            if (isset($_REQUEST['submit']) && $_REQUEST['contador'] < count($arr) - 1) {
+                $contador = $_REQUEST['contador'];
+                $aciertos = $_REQUEST['aciertos'];
+                $errores = $_REQUEST['errores'];
+                $infinitive = $_REQUEST['infinitive'];
+                $simple = $_REQUEST['simple'];
+                $participle = $_REQUEST['participle'];
+                if (
+                    $infinitive === $arr[$contador][1] &&
+                    $simple === $arr[$contador][2] &&
+                    $participle === $arr[$contador][3]
+                ) {
+                    $aciertos += 1;
+                } else {
+                    $errores += 1;
+                }
+                $contador += 1;
+                pintarArgumento(
+                    $contador,
+                    $aciertos,
+                    $errores,
+                    $infinitive,
+                    $simple,
+                    $participle
+                );
+            } elseif (!isset($_REQUEST['contador'])) {
+                pintarArgumento(0, 0, 0, "", "", "");
+            } else {
+                $contador = $_REQUEST['contador'];
+                $aciertos = $_REQUEST['aciertos'];
+                $errores = $_REQUEST['errores'];
+                $infinitive = $_REQUEST['infinitive'];
+                $simple = $_REQUEST['simple'];
+                $participle = $_REQUEST['participle'];
+                if (
+                    $infinitive === $arr[$contador][1] &&
+                    $simple === $arr[$contador][2] &&
+                    $participle === $arr[$contador][3]
+                ) {
+                    $aciertos += 1;
+                } else {
+                    $errores += 1;
+                }
+                echo "Aciertos: $aciertos<br>";
+                echo "Errores: $errores<br>";
+            }
+            ?>
+        </div>
 </body>
+
 </html>
--- ejercicio7/php/utility_functions.php
+++ ejercicio7/php/utility_functions.php
@@ -1,29 +1,19 @@
 <?php
-function pintar(){
-    $url = $_SERVER['PHP_SELF'] . "?contador=0&aciertos=0&errores=0&infinitive=\"\"&simple=\"\"&participle=\"\"";
-    echo $_SERVER['PHP_SELF'];
-    echo <<<EOD
-    <form class="form" method="post" action="$url">
-        <input type="submit" class="submit" value="Start" name='submit'>
-    </form>
-EOD;
-}
-
-function pintarArgumento($arr, $num){
-    $url = $_SERVER['PHP_SELF'] . "?contador=$num&aciertos=0&errores=0&infinitive=\"\"&simple=\"\"&participle=\"\"";
+function pintarArgumento($contador, $aciertos, $errores, $infinitive, $simple, $participle)
+{
+    include("verbos.php");
+    $url = $_SERVER['PHP_SELF'] .
+        "?contador=$contador&aciertos=$aciertos&errores=$errores" .
+        "&infinitive=$infinitive&simple=$simple&participle=$participle";
     echo <<<EOD
     <div class="box-verb">
-        <span class="box-verb-primary">{$arr[$num][0]}</span><br>
+        <span class="box-verb-primary">{$arr[$contador][0]}</span><br>
         <form class="form" method="post" action="$url">
             <input type="text" class="input-box" name="infinitive" placeholder="Infinitive" required></input>
             <input type="text" class="input-box" name="simple" placeholder="Past Simple" required></input>
             <input type="text" class="input-box" name="participle" placeholder="Past Participle" required></input>
-            <input type="hidden" name="aciertos" value="{$_REQUEST['aciertos']}">
-            <input type="hidden" name="errores" value="{$_REQUEST['errores']}">
-            <input type="submit" class="submit" value="Send">
+            <input type="submit" class="submit" value="Next" name='submit'>
         </form>
     </div>
 EOD;
 }
-
-?>
