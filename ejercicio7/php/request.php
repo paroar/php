4d947925bb2ffc42025435dccd8519523a37998c
paroar <prodriguezarmida@gmail.com>
2019-10-05 14:37:36 +0200

    deleted ejercicios[0-9]

--- ejercicio7/php/request.php
+++
@@ -1,8 +0,0 @@
-<?php
-$index = $_REQUEST['index'];
-$aciertos = $_REQUEST['aciertos'];
-$errores = $_REQUEST['errores'];
-$infinitive = $_REQUEST['infinitive'];
-$simple = $_REQUEST['simple'];
-$participle = $_REQUEST['participle'];
-?>
--- ejercicio9/php/clean.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("./utility_functions.php");
-if(isset($_REQUEST['text'])){
-    $ys = controla_entrada($_REQUEST['text']);
-    echo $ys;
-}else{
-    header("location: http://localhost/php/ejercicio9");
-}
-?>
