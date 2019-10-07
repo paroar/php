59e135e6b39ae12feb855c1a9f3c610c176c5322
paroar <prodriguezarmida@gmail.com>
2019-10-07 18:29:07 +0200

    stop-btn functionality & css

--- ejercicios/verbos-ingles/php/request.php
+++ ejercicios/verbos-ingles/php/request.php
@@ -1,7 +1,12 @@
 <?php
 require('verbos.php');
 require('utility_functions.php');
-if (isset($_POST['submit'])) {
+if (isset($_POST['stop'])) {
+    $length = count($arr);
+    $aciertos = $_GET['aciertos'];
+    $errores = $_GET['errores'];
+    header("location: ../index.php?index=$length&aciertos=$aciertos&errores=$errores");
+} else{
     $infinitive = $_POST['infinitive'];
     $simple = $_POST['simple'];
     $participle = $_POST['participle'];
--- ejercicios/verbos-ingles/php/utility_functions.php
+++ ejercicios/verbos-ingles/php/utility_functions.php
@@ -17,8 +17,9 @@ function pintar_formulario($index, $aciertos, $errores)
     <form class="form" method="post" action="php/request.php?index=$index&aciertos=$aciertos&errores=$errores">
         <input type="text" class="input-box" name="infinitive" placeholder="Infinitive" required>
         <input type="text" class="input-box" name="simple" placeholder="Past Simple" required>
-        <input type="text" class="input-box" name="participle" placeholder="Past Participle" required>
-        <input type="submit" class="submit" value="Next" name="submit">
+        <input type="text" class="input-box" name="participle" placeholder="Past Participle" required><br>
+        <input type="submit" class="submit next" value="Next" name="submit">
+        <input type="submit" class="submit stop" value="Stop" name="stop">
     </form>
 EOD;
 }
