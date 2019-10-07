c8944cba4f21ece0b2c282952e336482eebea4ca
paroar <prodriguezarmida@gmail.com>
2019-10-07 18:29:07 +0200

    stop-btn functionality & css

--- ejercicios/verbos-ingles/php/utility_functions.php
+++ ejercicios/verbos-ingles/php/utility_functions.php
@@ -17,8 +17,11 @@ function pintar_formulario($index, $aciertos, $errores)
     <form class="form" method="post" action="php/request.php?index=$index&aciertos=$aciertos&errores=$errores">
         <input type="text" class="input-box" name="infinitive" placeholder="Infinitive" required>
         <input type="text" class="input-box" name="simple" placeholder="Past Simple" required>
-        <input type="text" class="input-box" name="participle" placeholder="Past Participle" required>
-        <input type="submit" class="submit" value="Next" name="submit">
+        <input type="text" class="input-box" name="participle" placeholder="Past Participle" required><br>
+        <input type="submit" class="submit next" value="Next" name="submit">
+    </form>
+    <form class="form" method="post" action="php/request.php?index=$index&aciertos=$aciertos&errores=$errores">
+        <input type="submit" class="submit stop" value="Stop" name="stop">
     </form>
 EOD;
 }
