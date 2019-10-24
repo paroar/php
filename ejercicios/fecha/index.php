b3cf77084a1524b21b735e4c2edc5e792bf92246
paroar <prodriguezarmida@gmail.com>
2019-10-24 11:09:39 +0200

    calendarMonth

--- ejercicios/fecha/index.php
+++ ejercicios/fecha/index.php
@@ -5,16 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 
 <body>
     <?php
     require('./php/dates.php');
+    /*
     echo "Fecha de hoy: " . getDateTime('l d/m/Y') . "<br>";
     echo "Fecha de mañana: " . getDateTime('l d/m/Y', '+1 day') . "<br>";
     echo "Fecha del próximo Lunes: " . getDateTime('l d/m/Y', 'next monday') . "<br>";
-    echo "Hora de hoy: " . getDateTime('l d/m/Y H:i:s') . "<br>";
+    echo "Hora de hoy: " . getDateTime('l d/m/Y H:i:s') . "<br>";*/
+    echo "<div class='month'>";
+    echo calendarMonth("May",2019);
+    echo "</div>"
     ?>
 </body>
 
--- ejercicios/fecha/php/dates.php
+++ ejercicios/fecha/php/dates.php
@@ -1,8 +1,41 @@
 <?php
 
-function getDateTime($dateFormat, $dateModifier='+0 day')
+function getDateTime($dateModifier = '+0 day', $dateFormat = 'd/m/Y')
 {
     $date = new DateTime();
     $date->modify($dateModifier);
     return $date->format($dateFormat);
+}
+
+function arrayDaysMonth($month, $year)
+{
+    $arr = [];
+    $date = new DateTime("$month $year");
+    array_push($arr, $date->format('l'));
+    $days = $date->format('t');
+    for($i = 1; $i<=$days; $i++){
+        array_push($arr, $i);
+    }
+    return $arr;
+}
+
+function calendarMonth($month, $year){
+    $week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    $days = arrayDaysMonth($month,$year);
+    for($i=0; $i<count($week);$i++){
+        echo "<div class='week'><p>$week[$i]</p></div>";
+    }
+    echo "<br>";
+    for($i=0; $i<count($days);$i++){
+        if($days[0] == $week[$i]){
+            break;
+        }
+        echo "<div class='day'>  </div>";
+    }
+    for($i=1; $i<count($days);$i++){
+        echo "<div class='day'><p>$i</p></div>";
+    }
+
+
+
 }
