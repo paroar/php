a1c1e4faf27025964980c34f41e3e9e2ebea38f7
paroar <prodriguezarmida@gmail.com>
2019-10-27 00:09:36 +0200

    display flex auto

--- ejercicios/calendario/index.php
+++ ejercicios/calendario/index.php
@@ -2,24 +2,24 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
 </head>
 
 <body>
-    <?php
-    require('./php/dates.php');
-    require('./php/utilities.php');
-    if(!isset($_POST['submit'])){
-        paintForm();
-    }else{
-        paintForm();
-        control();
-    }
-    ?>
+  <?php
+  require('./php/dates.php');
+  require('./php/utilities.php');
+  if (!isset($_POST['submit'])) {
+    paintForm();
+  } else {
+    paintForm();
+    control();
+  }
+  ?>
 </body>
 
 </html>
