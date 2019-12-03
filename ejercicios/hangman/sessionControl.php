ec096b553b83388ba3e21b0cef35633237f1ed70
paroar <prodriguezarmida@gmail.com>
2019-12-03 13:18:06 +0100

    charword

--- ejercicios/hangman/sessionControl.php
+++ ejercicios/hangman/sessionControl.php
@@ -4,8 +4,9 @@ require_once("./functions.php");
 $_SESSION["counter"] = 0;
 $word = randomWord();
 $_SESSION["word"] = $word;
-$charWord = explode("", $word);
-$_SESSION["wordChars"] = array_unique($charWord);
+$charWord = str_split($word, "");
+//$_SESSION["wordChars"] = array_unique($charWord);
+printf($charWord);
 echo $_SESSION["word"];
 //header("Location: ./hangman.php");
 ?>
