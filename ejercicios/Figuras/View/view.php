9831aa2601e8bb22eef8042b196d5715bfaa0d06
paroar <prodriguezarmida@gmail.com>
2019-12-17 23:55:55 +0100

    Inicio Formulario progresivo

--- ejercicios/Figuras/View/view.php
+++ ejercicios/Figuras/View/view.php
@@ -3,7 +3,7 @@
 function paintFiguresShapesForm()
 {
     echo <<<EOD
-    <form action="./Controller/controller.php" method="POST" class="form">
+    <form action="./Controller/colorController.php" method="POST" class="form">
         <div class="checkbox">
         <input type="checkbox" name="shapes[]" value="2">Circle
         <input type="checkbox" name="shapes[]" value="3">Triangle
@@ -17,25 +17,25 @@ function paintFiguresShapesForm()
 EOD;
 }
 
-function paintFiguresSizesForm()
+function paintFiguresSizesForm($figures)
 {
     //TODO
+    echo '<form action="./Controller/controller.php" method="POST" class="form">';
+
+    foreach ($figures as $figure) {
+        echo  '<div class="checkbox">';
+        echo $figure->paintFigure;
+        echo "<input type='color' name={$figure->getSize()}>";
+        echo '</div>';
+    }
+
     echo <<<EOD
-    <form action="./Controller/controller.php" method="POST" class="form">
-        <div class="checkbox">
-        <input type="checkbox" name="shapes[]" value="2">Circle
-        <input type="checkbox" name="shapes[]" value="3">Triangle
-        <input type="checkbox" name="shapes[]" value="4">Square
-        <input type="checkbox" name="shapes[]" value="5">Pentagon
-        </div>
-        <input type="number" name="size" placeholder="Size" min="200" required>
-        <input type="color" name="color">
         <input type="submit" name="shape" value="Send">
     </form>
 EOD;
 }
 
-function paintFiguresColorsForm()
+function paintFiguresColorsForm($figures)
 {
     //TODO
     echo <<<EOD
