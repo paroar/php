b9329cd55292922c28aff0558119ecf5c5b2db68
paroar <prodriguezarmida@gmail.com>
2019-12-18 20:24:58 +0100

    Cleaning

--- ejercicios/Figuras/View/view.php
+++ ejercicios/Figuras/View/view.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function paintFiguresShapesForm()
 {
   $shapes = [
@@ -24,32 +22,3 @@ function paintFiguresShapesForm()
     </form>
 EOD;
 }
-
-// function paintFiguresShapesForm()
-// {
-//     echo <<<EOD
-//     <form action="./Controller/controller.php" method="POST" class="form">
-//         <div class="checkbox">
-//             <input type="checkbox" name="shapes[]" value="2"><span class="figureName">Circle</span>
-//             <input type="color" name="circleColor">
-//             <input type="number" name="circleSize" placeholder="Size" min="100" max="300" value="200">
-//         </div>
-//         <div class="checkbox">
-//             <input type="checkbox" name="shapes[]" value="3"><span class="figureName">Triangle</span>
-//             <input type="color" name="triangleColor">
-//             <input type="number" name="triangleSize" placeholder="Size" min="100" max="300" value="200">
-//         </div>
-//         <div class="checkbox">
-//             <input type="checkbox" name="shapes[]" value="4"><span class="figureName">Square</span>
-//             <input type="color" name="squareColor">
-//             <input type="number" name="squareSize" placeholder="Size" min="100" max="300" value="200">
-//         </div>
-//         <div class="checkbox">
-//             <input type="checkbox" name="shapes[]" value="5"><span class="figureName">Pentagon</span>
-//             <input type="color" name="pentagonColor">
-//             <input type="number" name="pentagonSize" placeholder="Size" min="100" max="300" value="200">
-//         </div>
-//         <input type="submit" name="submit" value="Send">
-//     </form>
-// EOD;
-// }
