c4af1cf3538e92068eeffa54cf96e47870745804
paroar <prodriguezarmida@gmail.com>
2020-01-10 11:10:37 +0100

    foreign keys

--- ejercicios/conexion-sql/create.php
+++ ejercicios/conexion-sql/create.php
@@ -29,68 +29,86 @@ function createTable($dbname, $table)
     }
 }
 
-$dbname = "libro";
+function importSqlQuery($query)
+{
+    try {
+        $db = new PDO(DBNAME, USER, PASSWORD);
+        $query = $db->prepare($query);
+        $query->execute();
+        echo "IMPORTACIÓN CON ÉXITO\n";
+    } catch (PDOException $e) {
+        echo "An error ocurred" . $e->getMessage();
+    }
+}
 
-$book =<<<SQL
-CREATE TABLE IF NOT EXISTS Book 
-(
-  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  isbn varchar(13),
-  title varchar(255),
-  author varchar(255),
-  stock smallint(5),
-  price float
-)
-SQL;
-
-$customer=<<<SQL
-CREATE TABLE IF NOT EXISTS Customer 
-(
-  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  firstname varchar(255),
-  surname varchar(255),
-  email varchar(255),
-  type enum('basic', 'premium')
-)
-SQL;
-
-$sale=<<<SQL
-CREATE TABLE IF NOT EXISTS Sale 
-(
-  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  customer_id int(10),
-  date datetime,
-  FOREIGN KEY(customer_id) REFERENCES Customer(id)
-)
-SQL;
-
-$borrowed_books =<<<SQL
-CREATE TABLE IF NOT EXISTS Borrowed_books 
-(
-  customer_id int(10),
-  book_id int(10),
-  start datetime,
-  end datetime,
-  FOREIGN KEY(customer_id) REFERENCES Customer(id),
-  FOREIGN KEY(book_id) REFERENCES Book(id)
-)
-SQL;
-
-$sale_book =<<<SQL
-CREATE TABLE IF NOT EXISTS Sale_book 
-(
-  book_id int(10),
-  sale_id int(10),
-  amount smallint(5),
-  FOREIGN KEY(sale_id) REFERENCES Sale(id),
-  FOREIGN KEY(book_id) REFERENCES Book(id)
-)
-SQL;
+$query = file_get_contents("libros.sql");
+importSqlQuery($query);
 
 
-createDB($dbname);
-createTable($dbname, $book);
-createTable($dbname, $customer);
-createTable($dbname, $sale);
-createTable($dbname, $borrowed_books);
-createTable($dbname, $sale_book);
+// $dbname = "libro";
+
+// $book =<<<SQL
+// CREATE TABLE IF NOT EXISTS Book 
+// (
+//   id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//   isbn varchar(13),
+//   title varchar(255),
+//   author varchar(255),
+//   stock smallint(5),
+//   price float
+// ) ENGINE=INNODB
+// SQL;
+
+// $customer=<<<SQL
+// CREATE TABLE IF NOT EXISTS Customer 
+// (
+//   id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//   firstname varchar(255),
+//   surname varchar(255),
+//   email varchar(255),
+//   type enum('basic', 'premium')
+// ) ENGINE=INNODB
+// SQL;
+
+// $sale=<<<SQL
+// CREATE TABLE IF NOT EXISTS Sale 
+// (
+//   id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//   customer_id int(10),
+//   date datetime,
+//   FOREIGN KEY(customer_id) REFERENCES Customer(id)
+// ) ENGINE=INNODB
+// SQL;
+
+// $borrowed_books =<<<SQL
+// CREATE TABLE IF NOT EXISTS Borrowed_books 
+// (
+//   customer_id int(10),
+//   book_id int(10),
+//   start datetime,
+//   end datetime,
+//   PRIMARY KEY(customer_id,book_id),
+//   FOREIGN KEY(customer_id) REFERENCES Customer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+//   FOREIGN KEY(book_id) REFERENCES Book(id) ON UPDATE CASCADE ON DELETE CASCADE
+// ) ENGINE=INNODB
+// SQL;
+
+// $sale_book =<<<SQL
+// CREATE TABLE IF NOT EXISTS Sale_book 
+// (
+//   book_id int(10),
+//   sale_id int(10),
+//   amount smallint(5),
+//   PRIMARY KEY(book_id,sale_id),
+//   FOREIGN KEY(sale_id) REFERENCES Sale(id) ON UPDATE CASCADE ON DELETE CASCADE,
+//   FOREIGN KEY(book_id) REFERENCES Book(id) ON UPDATE CASCADE ON DELETE CASCADE
+// ) ENGINE=INNODB
+//  SQL;
+
+
+// createDB($dbname);
+// createTable($dbname, $book);
+// createTable($dbname, $customer);
+// createTable($dbname, $sale);
+// createTable($dbname, $borrowed_books);
+// createTable($dbname, $sale_book);
---
+++ ejercicios/conexion-sql/libros.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS libros;
+use libros;
+CREATE TABLE IF NOT EXISTS Book 
+(
+  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  isbn varchar(13),
+  title varchar(255),
+  author varchar(255),
+  stock smallint(5),
+  price float
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS Customer 
+(
+  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  firstname varchar(255),
+  surname varchar(255),
+  email varchar(255),
+  type enum('basic', 'premium')
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS Sale 
+(
+  id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  customer_id int(10),
+  date datetime,
+  FOREIGN KEY(customer_id) REFERENCES Customer(id)
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS Borrowed_books 
+(
+  customer_id int(10),
+  book_id int(10),
+  start datetime,
+  end datetime,
+  PRIMARY KEY(customer_id,book_id),
+  FOREIGN KEY(customer_id) REFERENCES Customer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(book_id) REFERENCES Book(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS Sale_book 
+(
+  book_id int(10),
+  sale_id int(10),
+  amount smallint(5),
+  PRIMARY KEY(book_id,sale_id),
+  FOREIGN KEY(sale_id) REFERENCES Sale(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(book_id) REFERENCES Book(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=INNODB;
