75fa4e7f1a43f54f69a52887ce99b173f78aa5b5
paroar <prodriguezarmida@gmail.com>
2020-02-12 22:44:17 +0100

    adding form to xml

--- ejercicios/xml/src/index.php
+++ ejercicios/xml/src/index.php
@@ -48,7 +48,6 @@
         )
     );
 
-<<<<<<< HEAD
 
     // create simpleXML object
     $xml = new SimpleXMLElement("<?xml version=\"1.0\"?><SearchHotels></SearchHotels>");
@@ -77,41 +76,6 @@
             }
         }
     }
-=======
-function array_to_xml($array, &$xml_user_info)
-{
-    foreach ($array as $key => $value) {
-        if (is_array($value)) {
-            if (!is_numeric($key)) {
-                $subnode = $xml_user_info->addChild("$key");
-                array_to_xml($value, $subnode);
-            } else {
-                $subnode = $xml_user_info->addChild("item$key");
-                array_to_xml($value, $subnode);
-            }
-        } else {
-            $xml_user_info->addChild("$key", htmlspecialchars("$value"));
-        }
-    }
-}
-    //creating object of SimpleXMLElement
-$xml_user_info = new SimpleXMLElement("<?xml version='1.0'?><user_info></user_info>");
-
-//function call to convert array to xml
-array_to_xml($users_array, $xml_user_info);
-
-//saving generated xml file
-$xml_file = $xml_user_info->asXML('users.xml');
-
-//success and error message based on xml creation
-if ($xml_file) {
-    echo 'XML file have been generated successfully.';
-} else {
-    echo 'XML file generation error.';
-}
-
-
->>>>>>> xml
     ?>
 </body>
 
---
+++ ejercicios/xml/src/js/submit.js
@@ -0,0 +1,17 @@
+document.getElementById("submit").addEventListener("click", submitMovie)
+
+async function submitMovie(e) {
+    e.preventDefault();
+    
+    const form = document.getElementById("insertMovieForm");    
+    const data = new FormData(form);
+    data.append("submit","");
+    const res = await fetch("./src/php/index.php", {
+        method: 'POST',
+        body:data
+    }
+    )
+    const text = await res.json();
+    console.log("res",text);
+}
+
