ea48d04c6712b38964d1fd1bfbdab36bc2ef9a0e
paroar <prodriguezarmida@gmail.com>
2020-02-13 11:13:39 +0100

    XmlSaving

--- ejercicios/xml/src/php/Model/Xml.php
+++ ejercicios/xml/src/php/Model/Xml.php
@@ -6,30 +6,41 @@ class Xml
 
     public function __construct($path)
     {
-        $this->xml = simplexml_load_file($path);
+        try {
+            $this->xml = simplexml_load_file($path);
+        } catch (Throwable $th) {
+            return $th;
+        }
     }
 
-    public function add_movie($title, $names, $actors, $plot, $thumbs, $stars)
-    {
-        $characters = ``;
-        for ($i=0; $i < count($actors); $i++) { 
-            $characters .= `
-            <character>
-                <name>{$names[$i]}</name>
-                <actor>{$actors[$i]}</actor>
-            </character>
-            `;
-        }
+    public function getXml(){
+        return $this->xml;
+    }
 
-        $movie = `
-        <movie>
-            <title>{$title}</title>
-            <characters>{$characters}</characters>
-            <plot>{$plot}</plot>
-            <rating type="thumbs">{$thumbs}</rating>
-            <rating type="stars">{$stars}</rating>
-        </movie>
-        `;
-        //TODO
+    public function add_movie($title, $names, $actors, $plot, $thumbs, $stars, $submit)
+    {
+        try {
+            $movie = $this->xml->movies;
+
+            $movie->addChild("title", $title);
+
+            $characters = $movie->addChild("characters");
+            for ($i = 0; $i < count($actors); $i++) {
+                $character = $characters->addChild("character");
+                $character->addChild("name", $names[$i]);
+                $character->addChild("actor", $actors[$i]);
+            }
+
+            $movie->addChild("plot", $plot);
+            $thumbs = $movie->addChild("rating", $thumbs);
+            $thumbs->addAttribute("type", "thumbs");
+            $stars = $movie->addChild("rating", $stars);
+            $stars->addAttribute("type", "stars");
+
+            $this->xml->saveXML()->save("../xml/mvies.xml");
+
+        } catch (Throwable $th) {
+            return $th;
+        }
     }
 }
